55f111eac634a1533fd5ebfee440c9d8d6311f6d
Jake C <181171904+JakeCDev@users.noreply.github.com>
2025-03-24 01:22:35 -0600

    removed startup debug mode used in testing

--- The_house/game_files/start_menu.py
+++ The_house/game_files/start_menu.py
@@ -21,12 +21,13 @@ import sys
 #Create start menu function
 def start_menu():
 
+    #SPECIAL DEBUG MODE!!!!!!!!!!!!
     #pre-launch debug shortcut, # it out when not in use.
-    debug_choice = input(f"\n{YELLOW}(Debug shortcut){RESET} Type{YELLOW} 'debug'{RESET} to enter debug mode, or press {GREEN}Enter{RESET} to continue: ").strip().lower()
-    if debug_choice == "debug":
-        debug_menu()
-        return  #Return to prevent the rest of the menu from loading
-
+    #debug_choice = input(f"\n{YELLOW}(Debug shortcut){RESET} Type{YELLOW} 'debug'{RESET} to enter debug mode, or press {GREEN}Enter{RESET} to continue: ").strip().lower()
+    #if debug_choice == "debug":
+        #debug_menu()
+        #return  #Return to prevent the rest of the menu from loading
+    #SPECIAL DEBUG MODE!!!!!!!!!!!
 
     #start intro/start menu music on launch
     play_music("intro_music.wav", volume=0.4)  #Start looping intro music
